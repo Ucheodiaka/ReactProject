3657356b06fb8c31470d002fc1d8d830c929aee0
Ucheodiaka <ucheodiaka@gmail.com>
2023-03-22 17:40:18 +0100

    styls setup

---
+++ little-lemon/src/Nav.js
@@ -0,0 +1,40 @@
+import Logo from './Logo.svg';
+
+function Nav() {
+  return (
+    <nav>
+      <div className="logo">
+        <img src={Logo} alt="little lemon logo" />
+      </div>
+      <div className="navbar-links">
+        <ul>
+          <li>
+            <a href="#hero">Home</a>
+          </li>
+          <li>
+            <a href="#features">About</a>
+          </li>
+          <li>
+            <a href="#testimonials">Menu</a>
+          </li>
+          <li>
+            <a href="#pricing">Reservation</a>
+          </li>
+          <li>
+            <a href="#footer">Order online</a>
+          </li>
+          <li>
+            <a href="#login">Log in</a>
+          </li>
+          <li>
+            <a href="#signup" className="cta-btn">
+              Sign up
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
