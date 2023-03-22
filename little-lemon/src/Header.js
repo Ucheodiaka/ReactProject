a71f19b58e0bb224ff97fa2eadaee32415d0765b
Ucheodiaka <ucheodiaka@gmail.com>
2023-03-22 23:13:28 +0100

    styled header section

--- little-lemon/src/Header.js
+++ little-lemon/src/Header.js
@@ -3,17 +3,19 @@ import food from './Assets/food.jpg';
 function Header() {
   return (
     <header>
-      <div className="left-side">
-        <h1>little lemon</h1>
-        <h2>Chicago</h2>
-        <p>
-          We are a family owned Mediterranean restaurant, focused on traditional
-          recipes served with a modern twist.
-        </p>
-        <button>reserve a table</button>
-      </div>
-      <div className="right-side">
-        <img src={food} alt="lovely food" />
+      <div className="hero-center">
+        <div className="hero-info">
+          <h1>little lemon</h1>
+          <h2>chicago</h2>
+          <p>
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <button className="hero-btn">reserve a table</button>
+        </div>
+        <div className="hero-img">
+          <img src={food} alt="lovely food" />
+        </div>
       </div>
     </header>
   );
